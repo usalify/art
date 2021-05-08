dee279454672a8243fd4736b30e0d968971ae0bb
Lucio Benini <iiriya@iiriya.com>
2021-05-08 17:47:03 +0200

    Fix: colors
    Colors, Variables and NPM settings.

---
+++ dist/variables.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const variables = {
+  dark: '#52493a',
+  light: '#e8e0ae',
+  accent: '#d4af37'
+};
+var _default = variables;
+exports.default = _default;
---
+++ src/variables.js
@@ -0,0 +1,7 @@
+const variables = {
+  dark: '#52493a',
+  light: '#e8e0ae',
+  accent: '#d4af37'
+};
+
+export default variables;
